ee431210cc05a498f617d13070a0fade551557f7
Krzysztof Czopp <chris.czopp@premote.in>
2018-04-14 16:10:20 +0100

    improve element renderer

--- src/render-v-dom-element.js
+++ src/render-v-dom-element.js
@@ -2,38 +2,40 @@ const createVDomElement = require('./create-v-dom-element');
 
 const renderVDomElement = (
   name,
-  triggerNestedRender = null,
+  triggerNestedRenderOrProps = null,
   _createVDomElement = createVDomElement
 ) => {
-  const isItTextNode = typeof name === 'string' && triggerNestedRender === null;
+  const isItTextNodeRendering = typeof name === 'string' && triggerNestedRenderOrProps === null;
+  const isItComponentRendering = typeof name === 'function';
 
-  if (isItTextNode) {
+  if (isItTextNodeRendering) {
     const text = name;
 
     return text;
   }
 
-  const props = {};
+  const isSecondArgNestedRenderHook = typeof triggerNestedRenderOrProps === 'function';
+  const props = isSecondArgNestedRenderHook ? {} : (triggerNestedRenderOrProps || {});
   const argsToBePassedToElementCreator = [name, props];
 
   // eslint-disable-next-line no-shadow
-  const nestedRender = (name, triggerRestedRender) => {
-    const nestedElement = renderVDomElement(name, triggerRestedRender, _createVDomElement);
+  const nestedRender = (name, triggerNestedRender) => {
+    const nestedElement = renderVDomElement(name, triggerNestedRender, _createVDomElement);
 
     argsToBePassedToElementCreator.push(nestedElement);
   };
 
-  const isItComponent = typeof name === 'function';
+  if (!isItComponentRendering) {
+    if (isSecondArgNestedRenderHook) {
+      triggerNestedRenderOrProps(props, nestedRender);
+    }
 
-  triggerNestedRender(props, nestedRender);
-
-  if (isItComponent) {
-    const component = name;
-
-    return component(props);
+    return createVDomElement(...argsToBePassedToElementCreator);
   }
 
-  return createVDomElement(...argsToBePassedToElementCreator);
+  const component = name;
+
+  return component(props);
 };
 
 module.exports = renderVDomElement;
