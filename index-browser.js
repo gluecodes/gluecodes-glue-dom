2f7be9e1ed09b8f506579175b116ff07dd379a6f
Krzysztof Czopp <chris.czopp@premote.in>
2018-04-14 14:46:03 +0100

    fix wrong import

--- index-browser.js
+++ index-browser.js
@@ -1,6 +1,6 @@
 const initDom = require('./src/init-dom');
 const renderVDomTree = require('./src/render-v-dom-tree');
-const updateDom = require('./src/init-dom');
+const updateDom = require('./src/update-dom');
 
 module.exports = {
   initDom,
