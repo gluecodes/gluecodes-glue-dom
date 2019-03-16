0e6a461f663fd0308c40a9b0a590806ba75e7451
Krzysztof Czopp <chris@glue.codes>
2019-03-16 17:57:20 +0000

    createVirtualDom enhancer

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const createRenderer = require('./src/create-renderer');
+const createVirtualDomEnhancer = require('./src/create-virtual-dom-enhancer');
 const initDom = require('./src/init-dom');
 const renderVDomTree = require('./src/render-v-dom-tree');
 const updateDom = require('./src/update-dom');
 
 module.exports = {
   createRenderer,
+  createVirtualDomEnhancer,
   initDom,
   renderVDomTree,
   updateDom
